61d7a73027962b2b7f5f9d5223fd8ffaa1ba116a
isso <issoisso3@gmail.com>
2020-02-29 01:11:09 +0900

    l

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -10,10 +19,11 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
   end
+
 end
